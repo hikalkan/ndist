c8788a17c261103a3b3bac218a4882306bbcc5e3
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2013-04-08 21:11:57 +0300

    Corrected sample services

---
+++ src/NDist/NDist.Core/NDist/Management/Objects/ServiceInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Hik.NDist.Common;
+
+namespace Hik.NDist.Management.Objects
+{
+    [Serializable]
+    public class ServiceInfo
+    {
+        public string Name { get; set; }
+
+        public RunningStatus RunningStatus { get; set; }
+    }
+}
